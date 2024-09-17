5c7b1b5effdffd0014c8e30c83839b3abd1f3a0e
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2024-09-18 01:22:04 +0600

    Create component Task

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 import './App.css';
 import AddTaskForm from './components/AddTaskForm/AddTaskForm.tsx';
+import Task from "./components/Task/Task.tsx";
 
 const App = () => (
     <>
     <AddTaskForm onAddTask = {()=> console.log('123')} onChangeText = {()=> console.log('123')}/>
+        <Task textOfTask = {"123"} onDeleteTaskById ={()=> console.log('123')}/>
       </>
 );
 
---
+++ src/components/Task/Task.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './task.css'
+interface ITask extends React.PropsWithChildren{
+    textOfTask: string;
+    onDeleteTaskById:React.MouseEventHandler;
+}
+
+const Task: React.FC<ITask> = ({textOfTask, onDeleteTaskById}) => {
+    return (
+        <div className="task-item">
+            <p className="text-task">{textOfTask}</p>
+            <button className="remove-btn" type="button" onClick={onDeleteTaskById}>Remove task</button>
+        </div>
+    );
+};
+
+export default Task;
