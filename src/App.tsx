3c8b86953a7a42e90db0fc36e1947d2e024cef96
Aleksandra Melnikova <Aleksa1992.14.11@mail.ru>
2024-09-18 12:21:45 +0600

    Add checkbox for every task

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
 
 import './App.css';
 import AddTaskForm from './components/AddTaskForm/AddTaskForm.tsx';
-import Task from "./components/Task/Task.tsx";
-import {useState} from "react";
+import Task from './components/Task/Task.tsx';
+import {useState} from 'react';
 
 interface ITask {
     text: string;
     id: string,
+    checked: boolean,
 }
-
 
 const App = () => {
 
     const [tasks, setTasks] = useState<ITask[]>([
-        {id: '123', text: "Buy milk"},
-        {id: '1234', text: "Walk with dog"},
-        {id: '12345', text: "Do homework"},
+        {id: '123', text: 'Buy milk', checked: false},
+        {id: '1234', text: 'Walk with dog', checked: false},
+        {id: '12345', text: 'Do homework', checked: false},
     ]);
 
     const [currentTasks, setCurrentTasks] = useState<ITask[]>([
-        {id: '', text: ""},
-    ])
+        {id: '', text: '', checked: false},
+    ]);
 
     const changeText = (e:React.ChangeEvent<HTMLInputElement>,index:number) => {
         console.log(e.target.value);
         const copyCurrentTasks = [...currentTasks];
-        const  copyCurrentTask = {...copyCurrentTasks[index]}
+        const  copyCurrentTask = {...copyCurrentTasks[index]};
         copyCurrentTask.id = (new Date().getTime()).toString();
         copyCurrentTask.text = e.target.value;
         copyCurrentTasks[index]=copyCurrentTask;
@@ -40,15 +40,20 @@ const App = () => {
         copyTask.text = currentTasks[0].text;
         copyTasks.push(copyTask);
         setTasks(copyTasks);
-    }
+    };
 
     const deleteTask = (id:string) =>{
-
         const copyTasks = tasks.filter(task => task.id !== id);
-        // let index = people.findIndex(person => person.id === id);
-//     const copyPeople = [...people];
-// copyPeople.splice(index,1);
         setTasks(copyTasks );
+    };
+    const changeCheckbox = (id:string) =>{
+        const index = tasks.findIndex(task => task.id === id);
+        const copyTasks = [...tasks];
+        const copyTask = {...tasks[index]};
+        copyTask.checked = !copyTask.checked;
+        copyTasks[index] = copyTask;
+        setTasks(copyTasks);
+
     };
     return(
     <>
@@ -58,13 +63,16 @@ const App = () => {
             <Task
                 key={task.id}
                 textOfTask={task.text}
-                onDeleteTaskById={() => deleteTask(task.id)}>
+                onDeleteTaskById={() => deleteTask(task.id)}
+                checked={task.checked}
+                onChangeCheckbox = {() => changeCheckbox(task.id)}
+            >
             </Task>
 
         ))}
         </div>
     </>
-    )
+    );
 };
 
 
--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -4,13 +4,16 @@ import './task.css';
 interface ITask extends React.PropsWithChildren{
     textOfTask: string;
     onDeleteTaskById:React.MouseEventHandler;
+    checked: boolean;
+    onChangeCheckbox:React.ChangeEventHandler;
 }
 
-const Task: React.FC<ITask> = ({textOfTask, onDeleteTaskById}) => {
+const Task: React.FC<ITask> = ({textOfTask, onDeleteTaskById, checked, onChangeCheckbox}) => {
     return (
         <div className="task-item">
             <p className="text-task">{textOfTask}</p>
             <button className="remove-btn" type="button" onClick={onDeleteTaskById}>Remove task</button>
+            <input type="checkbox" checked={checked} onChange={onChangeCheckbox}/>
         </div>
     );
 };
